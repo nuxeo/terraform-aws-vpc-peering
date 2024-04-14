9c2f496964e41a1a8ed5f3c4c110c73ce37b220a
Vikrama Vutakanti <34922854+vvikram05@users.noreply.github.com>
2024-04-15 01:50:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
-provider "aws" "acceptance_peer" {
+provider "aws" {
+  alias  = "acceptance_peer"
   region = var.acceptance_region
 }
 
@@ -9,14 +10,6 @@ resource "aws_vpc_peering_connection" "default" {
   peer_region = var.acceptance_region
   auto_accept = var.auto_accept
 
-  accepter {
-    allow_remote_vpc_dns_resolution = var.acceptor_allow_remote_vpc_dns_resolution
-  }
-
-  requester {
-    allow_remote_vpc_dns_resolution = var.requestor_allow_remote_vpc_dns_resolution
-  }
-
   tags = module.this.tags
 
   timeouts {
@@ -24,6 +17,21 @@ resource "aws_vpc_peering_connection" "default" {
     update = var.update_timeout
     delete = var.delete_timeout
   }
+}
+
+resource "aws_vpc_peering_connection_accepter" "this" {
+  provider                  = aws.acceptance_peer # Use the provider configuration for the destination region
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[0].id
+  auto_accept               = true
+}
+
+resource "aws_vpc_peering_connection_options" "this" {
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[0].id
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
+  provider   = aws.acceptance_peer # Use the provider configuration for the destination region
+  depends_on = [aws_vpc_peering_connection_accepter.this]
 }
 
 # Lookup requestor VPC so that we can reference the CIDR
@@ -35,10 +43,10 @@ data "aws_vpc" "requestor" {
 
 # Lookup acceptor VPC so that we can reference the CIDR
 data "aws_vpc" "acceptor" {
-  count = module.this.enabled ? 1 : 0
-  provider = aws.peer
-  id    = var.acceptor_vpc_id
-  tags  = var.acceptor_vpc_tags
+  count    = module.this.enabled ? 1 : 0
+  provider = aws.acceptance_peer
+  id       = var.acceptor_vpc_id
+  tags     = var.acceptor_vpc_tags
 }
 
 data "aws_route_tables" "requestor" {
@@ -48,10 +56,10 @@ data "aws_route_tables" "requestor" {
 }
 
 data "aws_route_tables" "acceptor" {
-  count  = module.this.enabled ? 1 : 0
-  vpc_id = join("", data.aws_vpc.acceptor[*].id)
-  provider = aws.peer
-  tags   = var.acceptor_route_table_tags
+  count    = module.this.enabled ? 1 : 0
+  vpc_id   = join("", data.aws_vpc.acceptor[*].id)
+  provider = aws.acceptance_peer
+  tags     = var.acceptor_route_table_tags
 }
 
 locals {
@@ -78,6 +86,6 @@ resource "aws_route" "acceptor" {
   route_table_id            = element(distinct(sort(data.aws_route_tables.acceptor[0].ids)), ceil(count.index / length(local.requestor_cidr_blocks)))
   destination_cidr_block    = local.requestor_cidr_blocks[count.index % length(local.requestor_cidr_blocks)]
   vpc_peering_connection_id = join("", aws_vpc_peering_connection.default[*].id)
-  provider = aws.peer
+  provider                  = aws.acceptance_peer
   depends_on                = [data.aws_route_tables.acceptor, aws_vpc_peering_connection.default]
 }
